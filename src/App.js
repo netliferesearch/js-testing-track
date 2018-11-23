ec7fe1f1016d7a12ba2302646ea63a2baea05fda
Nils Norman Haukås <mail@nilsnh.no>
2018-11-23 12:59:53 +0100

    setup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Fagspor for javascript testing</h1>
+        <p>
+          If you like it you better put a regression test on it.{" "}
+          <span role="img" aria-label="ring">
+            💍
+          </span>
+        </p>
+        <p>Eksempler: </p>
+        <ul>
+          <li>
+            <a href="https://github.com/netliferesearch/valueaccounting">
+              En test lar deg gå langt.
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/netliferesearch/u4-no/tree/master/frontend/tests">
+              Disse testene har gjort oss godt.
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/netliferesearch/dfo-graphql/blob/master/src/services/test/elastic.test.js#L59">
+              Kirugisk datainngrep med unit test.
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/netliferesearch/forecast-gcalendar">
+              Sett sideeffekter til sides.
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/netliferesearch/foslive-node/blob/master/index.e2e.test.js">
+              Generer tester.
+            </a>
+          </li>
+        </ul>
+        <p>Testfilosofi.</p>
+        <ol>
+          <li>
+            La bibliotekene teste seg sjølv. Test din egen kode. Test det som du
+            er usikker på eller synes er komplisert.
+          </li>
+          <li>Unit testing vs integrasjonstesting.</li>
+          <li>Viss du er glad i koden putt en regresjonstest på den.</li>
+        </ol>
       </div>
     );
   }

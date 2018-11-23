0cf42fd133f2dd2ecce8bcc29f5252e53c07ad17
thomaslgj <thomas@netliferesearch.com>
2018-11-23 14:03:17 +0100

    Add Menu component

---
+++ src/Menu.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./Menu.css";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: false };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(e) {
+    console.log("test");
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="menu-button" onClick={this.toggleMenu}>
+          <div className="menu-button__bar" />
+          <div className="menu-button__bar" />
+          <div className="menu-button__bar" />
+        </button>
+        {this.state.isToggleOn ? (
+          <div className="menu-content">Menu stuff goes here</div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default Menu;
